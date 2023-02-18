e53ad8d16ba78971a99c285757cc3406a74f37d2
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-18 23:48:11 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -1,12 +1,25 @@
 <?php
-  // 讀取JSON檔案中的資料
-  $jsonString = file_get_contents('data.json');
-  $data = json_decode($jsonString, true);
+  if(isset($_POST['data'])) {
+    // 取得 POST 傳送過來的資料
+    $data = json_decode($_POST['data'], true);
 
-  // 將數據輸出到網頁中
-  foreach ($data as $item) {
-    echo '<p>單號：' . $item['no'] . '</p>';
-    echo '<p>用餐場所：' . $item['place'] . '</p>';
-    echo '<p>等候狀態：' . $item['status'] . '</p>';
-  }
+    // 讀取原有的 JSON 檔案
+    $json = file_get_contents('data.json');
+
+    // 將 JSON 轉換成 PHP 陣列
+    $arr_data = json_decode($json, true);
+
+    // 新增資料到陣列中
+    array_push($arr_data, $data);
+
+    // 將陣列轉換成 JSON
+    $json_data = json_encode($arr_data, JSON_PRETTY_PRINT);
+
+    // 寫入檔案
+    if(file_put_contents('data.json', $json_data)) {
+        echo '資料已成功存入檔案！';
+    } else {
+        echo '發生錯誤，無法存入檔案。';
+    }
+}
 ?>
