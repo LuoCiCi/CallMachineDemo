abacf5a6073d647bdc9aff3202def9836e29eced
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-20 22:24:14 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Headers: Origin, Methods, Content-Type, Authorization");
+
     // 讀取 POST 資料，並解析為 JSON 格式
 $data = json_decode(file_get_contents('php://input'), true);
 
