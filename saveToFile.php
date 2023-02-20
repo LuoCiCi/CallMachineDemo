c085e187436f45715da20dae7ca587abd3918b7d
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-20 22:35:07 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -4,10 +4,10 @@
     header("Access-Control-Allow-Headers: Origin, Methods, Content-Type, Authorization");
 
     // 讀取 POST 資料，並解析為 JSON 格式
-$data = json_decode(file_get_contents('https://luocici.github.io/CallMachineDemo/data.json'), true);
+    $url = 'https://luocici.github.io/CallMachineDemo/data.json';
+    $json = file_get_contents($url);
+    $data = json_decode($json, true);
 
-// 要寫入的 JSON 資料
-$json_data = json_encode($data);
 
 // 開啟檔案，並寫入資料
 $file = fopen('data.json', 'w');
