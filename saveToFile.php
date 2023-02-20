987a515ad0935b642e8ed052dd53a61a20bffadd
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-20 21:10:59 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -2,10 +2,13 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: *");
     header("Access-Control-Allow-Headers: Origin, Methods, Content-Type, Authorization");
+header("Content-Type:application/json; charset=utf-8");
 
     $data = $_POST['text'];
     $file = fopen('https://luocici.github.io/CallMachineDemo/data.json', 'w');
     fwrite($file, $data);
     fclose($file);
-	echo json_encode(array('OK --- text' => $_POST['text']));
+
+    ob_clean();
+    echo json_encode(array('OK --- text' => $_POST['text']));
 ?>
