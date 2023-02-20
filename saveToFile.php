f5a4ec7d2c62a201b62957962e044a1e8aff0d97
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-20 17:05:13 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -1,6 +1,6 @@
 <?php
-$data = array('key1' => 'value1', 'key2' => 'value2');
-$json = json_encode($data);
-$callback = $_GET['callback'];
-echo $callback . '(' . $json . ')';
+    $data = $_POST['text'];
+    $file = fopen('data.txt', 'w');
+    fwrite($file, $data);
+    fclose($file);
 ?>
