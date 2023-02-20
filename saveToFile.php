b87c8819b9b9567eea6a7adc44fa93672083b366
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2023-02-20 21:33:04 +0800

    Update saveToFile.php

--- saveToFile.php
+++ saveToFile.php
@@ -6,7 +6,7 @@ header("Content-Type:application/json; charset=utf-8");
 
     $data = $_POST['text'];
     $file = fopen('https://luocici.github.io/CallMachineDemo/data.json', 'w');
-    fwrite($file, $data);
+    fwrite($file, json_decode($data));
     fclose($file);
 
 //     ob_clean();
